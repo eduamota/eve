a5851b39fbe3d7bf4d05fdd46027a0b6e4f8d9fd
Eduardo <eduardo382@gmail.com>
2017-09-18 16:24:57 -0700

    Add client model to manage client configuration

---
+++ eve_site/clients/admin.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+
+# Register your models here.
+from .models import Client, Language, Phone_Number, Payment_Model
+
+admin.site.register(Client)
+admin.site.register(Language)
+admin.site.register(Phone_Number)
+admin.site.register(Payment_Model)
--- text_processing/brain.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jul 31 15:21:48 2017
-
-@author: emota
-"""
-
-from class_vis import prettyPicture
