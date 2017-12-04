079ca53a5c4d7dc866a6973d8b505d6df7ff37ed
Eduardo <emota@hyperwallet.com>
2017-12-04 11:15:44 -0800

    "Remove virtual env"

--- env_eve/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\emota\documents\hyperwallet\projects\eve\env_eve\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
